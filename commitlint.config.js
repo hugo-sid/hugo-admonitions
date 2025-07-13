cf5e7cfb438c6495f8b03e80d9ee6b0d12ec6a1d
KKKZOZ <1206668472@qq.com>
2025-07-13 09:38:12 +0800

    ci: add i18n type support

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,23 @@
 module.exports = {
-    extends: ['@commitlint/config-conventional'],
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2, // Level: Error
+      "always",
+      [
+        "build",
+        "chore",
+        "ci",
+        "docs",
+        "feat",
+        "fix",
+        "perf",
+        "refactor",
+        "revert",
+        "style",
+        "test",
+        "i18n",
+      ],
+    ],
+  },
 };
